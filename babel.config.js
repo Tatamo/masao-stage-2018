480fe687b3d8bed7dbfe9a762cfd436371093be4
Tatamo <nsorder@gmail.com>
2018-09-05 00:10:21 +0900

    reformat config files to follow .editorconfig and prettier rules

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = api => {
-    api.cache(true);
-    const presets = ["@babel/preset-env"];
-    const plugins = [];
+	api.cache(true);
+	const presets = ["@babel/preset-env"];
+	const plugins = [];
 
-    return {
-        presets,
-        plugins
-    };
+	return {
+		presets,
+		plugins
+	};
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-    mode: "development",
-    entry: "./src/index.ts",
-    output: {
-        path: __dirname + "/dist",
-        filename: "bundle.js"
-    },
-    resolve: {
-        extensions: [".ts", ".js"]
-    },
-    module: {
-        rules: [
-	        {
-		        test: /\.ts$/,
-		        enforce: 'pre',
-		        loader: 'tslint-loader',
-		        options: {
-			        typeCheck: true,
-			        fix: true,
-		        }
-	        },
-            {
-                test: /\.(ts|js)$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {test: /\.ts$/, loader: "ts-loader"}
-        ]
-    }
+	mode: "development",
+	entry: "./src/index.ts",
+	output: {
+		path: __dirname + "/dist",
+		filename: "bundle.js"
+	},
+	resolve: {
+		extensions: [".ts", ".js"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				enforce: "pre",
+				loader: "tslint-loader",
+				options: {
+					typeCheck: true,
+					fix: true
+				}
+			},
+			{
+				test: /\.(ts|js)$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			},
+			{ test: /\.ts$/, loader: "ts-loader" }
+		]
+	}
 };
