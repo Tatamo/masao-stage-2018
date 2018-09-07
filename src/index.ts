af0b4529f04c5bb274be7211871b78763c7135b2
Tatamo <nsorder@gmail.com>
2018-09-07 18:27:21 +0900

    add launch.ts to separate game constructing code

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,6 @@
 import { format } from "masao";
 import json from "./game.json";
-import { Graphics } from "./definitions/graphics";
-import { Main } from "./main";
-import { OnImageLoadedCallbackExtension } from "./extensions/imageloadcallback";
-import { InitCallbackExtension } from "./extensions/initcallback";
-import { LoadingScreenSuppressorExtension } from "./extensions/loading_screen_suppressor";
-import { LoadCompleteWaiterExtension } from "./extensions/loadcompletewaiter";
-import * as PIXI from "pixi.js";
-
-// JSMasaoオブジェクトの型宣言を行っておく
-interface JSMasaoOptions {
-	extensions?: Array<object>;
-	userJSCallback?: (offscreen_g: Graphics, mode: number, view_x: number, view_y: number, ap: any) => void;
-	userHighscoreCallback?: (score: number) => void;
-	"advance-map"?: object;
-}
-
-declare class JSMasao {
-	constructor(params: object, id?: string, options?: JSMasaoOptions);
-}
+import { launch } from "./launch";
 
 // masao-json-formatからパラメータオブジェクトを取得
 const { params: p, "advanced-map": am } = format.load(json);
@@ -67,39 +49,4 @@ const params = {
 	filename_se_chizugamen: "assets/sounds/get.mp3"
 };
 
-const load_complete_waiter = new LoadCompleteWaiterExtension();
-const { init, onload, userjs } = (() => {
-	let main: Main | null = null;
-	return {
-		init: (mc: any, graphics: Graphics, jss: any) => {
-			PIXI.loader.add("pattern", "assets/images/pattern.gif");
-			PIXI.loader.load((loader: PIXI.loaders.Loader, resources: PIXI.loaders.ResourceDictionary) => {
-				load_complete_waiter.go();
-				main = new Main(jss, graphics, resources);
-			});
-
-			graphics._ctx.fillStyle = "#000000";
-			graphics._ctx.fillRect(0, 0, 512, 320);
-			graphics._ctx.fillStyle = "#ffffff";
-			graphics._ctx.font = "16px sans-serif";
-
-			graphics._ctx.fillText("ただいまファイルを読み込み中。しばらくお待ち下さい。", 32, 160);
-		},
-		onload: (mc: any, graphics: Graphics, jss: any) => {},
-		userjs: (graphics: Graphics, mode: number, view_x: number, view_y: number, ap: any) => {
-			main!.userJS(mode, view_x, view_y);
-		}
-	};
-})();
-// ゲームオブジェクトを作成
-// tslint:disable-next-line:no-unused-expression
-new JSMasao(params, undefined, {
-	userJSCallback: userjs,
-	extensions: [
-		new InitCallbackExtension(init),
-		new OnImageLoadedCallbackExtension(onload),
-		new LoadingScreenSuppressorExtension(),
-		load_complete_waiter
-	],
-	"advance-map": am
-});
+launch(params, am!);
---
+++ src/launch.ts
@@ -0,0 +1,60 @@
+import { LoadCompleteWaiterExtension } from "./extensions/loadcompletewaiter";
+import { Main } from "./main";
+import { Graphics } from "./definitions/graphics";
+import * as PIXI from "pixi.js";
+import { InitCallbackExtension } from "./extensions/initcallback";
+import { OnImageLoadedCallbackExtension } from "./extensions/imageloadcallback";
+import { LoadingScreenSuppressorExtension } from "./extensions/loading_screen_suppressor";
+
+// JSMasaoオブジェクトの型情報を宣言しておく
+interface JSMasaoOptions {
+	extensions?: Array<object>;
+	userJSCallback?: (offscreen_g: Graphics, mode: number, view_x: number, view_y: number, ap: any) => void;
+	userHighscoreCallback?: (score: number) => void;
+	"advance-map"?: object;
+}
+
+declare class JSMasao {
+	constructor(params: object, id?: string, options?: JSMasaoOptions);
+}
+
+/**
+ * ゲームを起動する
+ * @param params
+ * @param advancemap
+ */
+export function launch(params: object, advancemap: object) {
+	// ローディング周りの非同期処理が非常に煩雑なので汚いコードはここにまとめる
+	const load_complete_waiter = new LoadCompleteWaiterExtension();
+	let main: Main | null = null;
+	const init = (mc: any, graphics: Graphics, jss: any) => {
+		PIXI.loader.add("pattern", "assets/images/pattern.gif");
+		PIXI.loader.load((loader: PIXI.loaders.Loader, resources: PIXI.loaders.ResourceDictionary) => {
+			load_complete_waiter.go();
+			main = new Main(jss, graphics, resources);
+		});
+
+		graphics._ctx.fillStyle = "#000000";
+		graphics._ctx.fillRect(0, 0, 512, 320);
+		graphics._ctx.fillStyle = "#ffffff";
+		graphics._ctx.font = "16px sans-serif";
+
+		graphics._ctx.fillText("ただいまファイルを読み込み中。しばらくお待ち下さい。", 32, 160);
+	};
+	const onload = (mc: any, graphics: Graphics, jss: any) => {};
+	const userjs = (graphics: Graphics, mode: number, view_x: number, view_y: number, ap: any) => {
+		main!.userJS(mode, view_x, view_y);
+	};
+	// ゲームオブジェクトを作成
+	// tslint:disable-next-line:no-unused-expression
+	new JSMasao(params, undefined, {
+		userJSCallback: userjs,
+		extensions: [
+			new InitCallbackExtension(init),
+			new OnImageLoadedCallbackExtension(onload),
+			new LoadingScreenSuppressorExtension(),
+			load_complete_waiter
+		],
+		"advance-map": advancemap
+	});
+}
