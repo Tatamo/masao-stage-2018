31a5b377bdf9b6b570baf1334435510bb98c4437
Tatamo <nsorder@gmail.com>
2018-09-09 05:13:51 +0900

    add Resource.textures map to store custom textures

--- src/components/resource.ts
+++ src/components/resource.ts
@@ -1,16 +1,36 @@
 import * as PIXI from "pixi.js";
 export class Resource {
+	/**
+	 * パターン画像のテクスチャ
+	 * パターンコードで添え字アクセスして取得する
+	 */
 	get pattern(): Array<PIXI.Texture> {
 		return this._pattern;
 	}
+
+	/**
+	 * ファイルから読み込んだ画像のテクスチャ
+	 */
 	get images(): { [name: string]: PIXI.Texture } {
 		return this._images;
 	}
+
+	/**
+	 * 使いまわすために新しく生成したテクスチャを格納する
+	 */
+	get textures(): Map<string, PIXI.Texture> {
+		return this._textures;
+	}
+
+	/**
+	 * ファイルから読み込んだ音楽
+	 */
 	get sounds(): { [p: string]: HTMLAudioElement } {
 		return this._sounds;
 	}
 	private readonly _pattern: Array<PIXI.Texture>;
 	private readonly _images: { [name: string]: PIXI.Texture };
+	private readonly _textures: Map<string, PIXI.Texture>;
 	private readonly _sounds: { [name: string]: HTMLAudioElement };
 	constructor(resources: PIXI.loaders.ResourceDictionary) {
 		this._pattern = [];
@@ -25,6 +45,8 @@ export class Resource {
 			ending: resources["ending"].texture,
 			health_bar: resources["health_bar"].texture
 		};
+
+		this._textures = new Map();
 
 		const sounds = [
 			"bakuhatu",
