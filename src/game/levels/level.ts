2ad70c5314de4ccf228ff87e997a318d3414e5c9
Tatamo <nsorder@gmail.com>
2018-09-09 23:28:17 +0900

    add PIXI.Container and EntityContainer access to Level interface

--- src/game/levels/level.ts
+++ src/game/levels/level.ts
@@ -1,7 +1,11 @@
 import * as PIXI from "pixi.js";
 import { GameAPI } from "../api";
+import { Entity } from "../component/entities/entity";
+import { EntityContainer } from "../component/entities/container";
 
 export interface Level {
+	readonly stage: PIXI.Container;
+	add(entitiy: Entity): void;
 	/**
 	 * 毎フレームの更新処理
 	 */
@@ -20,10 +24,18 @@ export interface Level {
 }
 
 export abstract class AbstractLevel implements Level {
-	protected readonly stage: PIXI.Container;
+	get stage(): PIXI.Container {
+		return this._stage;
+	}
+	protected readonly entities: EntityContainer;
+	protected _stage: PIXI.Container;
 	protected constructor(protected readonly api: GameAPI) {
-		this.stage = new PIXI.Container();
+		this._stage = new PIXI.Container();
+		this.entities = new EntityContainer(api, this.stage);
 		this.api.root.addChild(this.stage);
+	}
+	add(entity: Entity) {
+		this.entities.add(entity);
 	}
 	abstract update(): void;
 	abstract render(): void;
--- src/game/levels/stage1.ts
+++ src/game/levels/stage1.ts
@@ -2,25 +2,22 @@ import { AbstractLevel } from "./level";
 import { HealthBar } from "../component/information/healthbar";
 import { MAX_HP } from "../../main";
 import { GameAPI } from "../api";
-import { EntityContainer } from "../component/entities/container";
 import { Boss1 } from "../component/entities/enemy/boss1";
 
 export class Stage1 extends AbstractLevel {
 	private readonly health_bar: HealthBar;
-	private readonly enemies: EntityContainer;
 	constructor(api: GameAPI) {
 		super(api);
 		this.health_bar = new HealthBar(api, this.stage, MAX_HP);
 		api.jss.setMyMaxHP(MAX_HP);
-		this.enemies = new EntityContainer(api, this.stage);
-		this.enemies.add(new Boss1(api, 384, 800 + 320));
+		this.entities.add(new Boss1(api, 384, 800 + 320));
 	}
 	update() {
 		this.health_bar.update();
-		this.enemies.update();
+		this.entities.update();
 	}
 	render() {
 		this.health_bar.render();
-		this.enemies.render();
+		this.entities.render();
 	}
 }
