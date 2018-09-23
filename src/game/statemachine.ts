7582c8b32ef580d2829f8fdf8a58ee86035b4985
Tatamo <nsorder@gmail.com>
2018-09-24 05:34:56 +0900

    now StateMachine has a call stack

--- src/game/statemachine.ts
+++ src/game/statemachine.ts
@@ -55,46 +55,95 @@ export abstract class AbstractState<P extends StateMachine> implements State<P> 
 	}
 }
 
+interface StateControlBlock<P extends StateMachine> {
+	done: boolean;
+	readonly loop: boolean;
+	state_changed_in_update: boolean;
+	readonly state: State<P>;
+	generator: IterableIterator<void>;
+}
+
 export class StateMachine {
 	get done(): boolean {
-		return this._done;
+		if (this.state_stack.length === 0) return true;
+		return this.state_stack[this.state_stack.length - 1].done;
 	}
-	private state: State<this> | null;
-	private flg_loop: boolean;
+	set done(value: boolean) {
+		if (this.state_stack.length === 0) return;
+		this.state_stack[this.state_stack.length - 1].done = value;
+	}
+	private get loop(): boolean {
+		if (this.state_stack.length === 0) return true;
+		return this.state_stack[this.state_stack.length - 1].loop;
+	}
+	private get state_changed_in_update(): boolean {
+		if (this.state_stack.length === 0) return false;
+		return this.state_stack[this.state_stack.length - 1].state_changed_in_update;
+	}
+	private set state_changed_in_update(value: boolean) {
+		if (this.state_stack.length === 0) return;
+		this.state_stack[this.state_stack.length - 1].state_changed_in_update = value;
+	}
+	private get state(): State<this> | null {
+		if (this.state_stack.length === 0) return null;
+		return this.state_stack[this.state_stack.length - 1].state;
+	}
+	private get generator(): IterableIterator<void> | null {
+		if (this.state_stack.length === 0) return null;
+		return this.state_stack[this.state_stack.length - 1].generator;
+	}
+	// private state: State<this> | null;
+	// private flg_loop: boolean;
 	// update()の実行中にsetStateが呼ばれたことを知るためのフラグ
-	private flg_state_changed_in_update: boolean;
-	private _done: boolean;
-	private generator: IterableIterator<void> | null;
+	// private flg_state_changed_in_update: boolean;
+	// private _done: boolean;
+	// private generator: IterableIterator<void> | null;
+	private readonly state_stack: Array<StateControlBlock<this>>;
 	constructor() {
-		this.state = null;
-		this.flg_loop = true;
-		this.flg_state_changed_in_update = false;
-		this._done = false;
-		this.generator = null;
+		this.state_stack = [];
+	}
+	private regenerate(): void {
+		const len = this.state_stack.length;
+		if (len === 0) return;
+		this.done = false;
+		this.state_stack[len - 1].generator = this.state_stack[len - 1].state.update();
 	}
 	setState(state: State<this>, loop: boolean = true): void {
-		this.state = state;
-		this.flg_loop = loop;
-		this.flg_state_changed_in_update = true;
-		this._done = false;
-		this.generator = state.update();
+		this.state_stack.length = 0;
+		this.pushState(state, loop);
+	}
+	pushState(state: State<this>, loop: boolean = true): void {
+		this.state_stack.push({
+			state,
+			loop,
+			done: false,
+			state_changed_in_update: true,
+			generator: state.update()
+		});
 		// 初期化
-		this.state.init();
+		this.state!.init();
+	}
+	popState() {
+		if (this.state_stack.length > 0) this.state_stack.length -= 1;
+		this.state_changed_in_update = true;
 	}
 	update(): void {
-		this.flg_state_changed_in_update = false;
+		this.state_changed_in_update = false;
 		// 状態が設定されていない場合は何もしない
 		if (this.state === null) return;
 		// ジェネレータを回して処理
 		if (this.generator !== null) {
 			// update()途中でstateが変わった場合はgeneratorも置き換わっているので何もしなくてよい
-			if (this.generator.next().done && this.flg_state_changed_in_update === false) {
-				if (this.flg_loop) {
-					// 終了後、loopフラグがONなら再度同じ処理を行う
-					this.generator = this.state.update();
-				} else {
-					this.generator = null;
-					this._done = true;
+			if (this.generator.next().done && this.state_changed_in_update === false) {
+				this.done = true;
+				// スタックをポップできるところまでポップする
+				while (this.state_stack.length > 0 && this.done) {
+					if (this.loop) {
+						// 終了後、loopフラグがONなら再度同じ処理を行う
+						this.regenerate();
+					} else {
+						this.state_stack.length -= 1;
+					}
 				}
 			}
 		}
