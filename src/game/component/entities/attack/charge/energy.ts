267a36b4f369d0a74d3d57af8530554461f0dbec
Tatamo <nsorder@gmail.com>
2018-10-01 23:36:38 +0900

    fix lag

--- src/game/component/entities/attack/charge/energy.ts
+++ src/game/component/entities/attack/charge/energy.ts
@@ -5,7 +5,6 @@ import { AbstractState } from "../../../../statemachine";
 import { easeOutCubic } from "../../../../../utils/easing";
 
 export class Energy extends Entity {
-	public filter: PIXI.filters.ColorMatrixFilter;
 	public readonly sprite: PIXI.Sprite;
 	constructor(
 		level: Level,
@@ -22,8 +21,7 @@ export class Energy extends Entity {
 		this.sprite = new PIXI.Sprite(resource.images["bullet_blue"]);
 		this.sprite.anchor.x = 0.5;
 		this.sprite.anchor.y = 0.5;
-		this.sprite.filters = [(this.filter = new PIXI.filters.ColorMatrixFilter()), new PIXI.filters.AlphaFilter()];
-		this.sprite.filters[1].blendMode = PIXI.BLEND_MODES.ADD;
+		this.sprite.blendMode = PIXI.BLEND_MODES.ADD;
 		const s = 0.25 * Math.random();
 		this.sprite.scale.x = 0.25 + s;
 		this.sprite.scale.y = 0.25 + s;
@@ -36,12 +34,6 @@ export class Energy extends Entity {
 
 namespace States {
 	export class Default<P extends Energy> extends AbstractState<P> {
-		private skip!: number;
-		init(): void {
-			// 最初の位相差を設定
-			this.skip = 0;
-			this.parent.filter.hue(this.skip);
-		}
 		*update(): IterableIterator<void> {
 			const dx = this.parent.to_x - this.parent.x;
 			const dy = this.parent.to_y - this.parent.y;
@@ -49,32 +41,9 @@ namespace States {
 			for (let i = 0; i < this.parent.timespan; i++) {
 				this.parent.x = this.parent.to_x - dx * (1 - easing(i / this.parent.timespan));
 				this.parent.y = this.parent.to_y - dy * (1 - easing(i / this.parent.timespan));
-				// this.parent.sprite.alpha = 0.05 + 0.1 * easing(i / this.parent.timespan);
+				this.parent.sprite.alpha = 0.05 + 0.1 * easing(i / this.parent.timespan);
 				yield;
 			}
-
-			/*
-			for (let i = 0; i < 360; i += 2) {
-				if (this.skip > 0) {
-					// 一番最初に一定の位相だけずらす
-					this.skip -= 2;
-					continue;
-				}
-				// this.parent.vx *= 0.95;
-				// this.parent.vy *= 0.95;
-				// this.parent.x += this.parent.vx;
-				// this.parent.y += this.parent.vy;
-				this.parent.sprite.alpha *= 0.95;
-
-				this.parent.filter.hue(i);
-				if (this.parent.sprite.alpha < 0.01) {
-					this.parent.kill();
-					return;
-				}
-				yield;
-			}
-			*/
 		}
-		render(): void {}
 	}
 }
